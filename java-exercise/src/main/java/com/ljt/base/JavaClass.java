b19160a7dbc4859723f63dd7f972e288366c0b54
JTL <1126088799@qq.com>
2021-08-06 21:13:10 +0800

    类学习

--- java-exercise/src/main/java/com/ljt/base/JavaClass.java
+++ java-exercise/src/main/java/com/ljt/base/JavaClass.java
@@ -1,2 +1,19 @@
-package com.ljt.base;public class JavaClass {
+package com.ljt.base;
+
+public class JavaClass {
+
+   /*Java 类学习*/
+    String breed;
+    int size;
+    String colour;
+    int age;
+    void eat(){
+     }
+    void run(){
+     }
+    void sleep(){
+     }
+    void name(){
+     }
 }
+
