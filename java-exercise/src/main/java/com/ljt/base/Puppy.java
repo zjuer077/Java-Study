34dc8419baa5959ad9d5517e7641aa7dd73f23bd
JTL <1126088799@qq.com>
2021-08-06 21:16:00 +0800

    创建对象

--- java-exercise/src/main/java/com/ljt/base/Puppy.java
+++ java-exercise/src/main/java/com/ljt/base/Puppy.java
@@ -1,2 +1,32 @@
-package com.ljt.base;public class Puppy {
+package com.ljt.base;
+
+public class Puppy {
+    int puppyAge;
+    public Puppy(String name){
+        System.out.println("小狗的名字是 ： " + name);
+    }
+
+    /* public void setPuppyAge(int age) {
+        puppyAge = age;
+    } */
+
+    public void setPuppyAge(int puppyAge) {
+        this.puppyAge = puppyAge;
+    }
+
+    public int getAge() {
+        System.out.println("小狗的年龄为 ： " + puppyAge );
+        return puppyAge;
+    }
+
+    public static void main(String[] args) {
+       /*创建对象*/
+        Puppy myPuppy = new Puppy("tommy");
+        /*通过方法来设定age */
+        myPuppy.setPuppyAge( 2 );
+        /*通过另外的方法获取age */
+        myPuppy.getAge( );
+        /*也可以像下面这样访问成员变量 */
+        System.out.println("变量值 ： " + myPuppy.puppyAge );
+    }
 }
