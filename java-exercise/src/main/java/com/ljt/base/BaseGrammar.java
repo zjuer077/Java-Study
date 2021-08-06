820b6253c15d4459b4bb3b0f8f5199ffcce43092
JTL <1126088799@qq.com>
2021-08-06 21:12:08 +0800

    语法学习

--- java-exercise/src/main/java/com/ljt/base/BaseGrammar.java
+++ java-exercise/src/main/java/com/ljt/base/BaseGrammar.java
@@ -1,7 +1,14 @@
 package com.ljt.base;
 
 public class BaseGrammar {
+    /*这是第一个Java程序
+     *他将输出 Hello World
+     * 这是一个多行注释的示例
+     */
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        //这是单行注释的实例
+        /*这个也是单行注释的实例*/
     }
 }
