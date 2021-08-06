22f9f883e5b5a9073ea30b56b7fdeef43c2ea31f
JTL <1126088799@qq.com>
2021-08-06 21:14:33 +0800

    构建方法学习

--- java-exercise/src/main/java/com/ljt/base/ObjectSubstruct.java
+++ java-exercise/src/main/java/com/ljt/base/ObjectSubstruct.java
@@ -1,2 +1,15 @@
-package com.ljt.base;public class ObjectSubstruct {
+package com.ljt.base;
+//class 的构造方法，在创建一个对象的时候，至少要调用一个构造方法。
+public class ObjectSubstruct {
+
+    public ObjectSubstruct(String name){
+        //这个构造器仅有一个参数：name
+        /*此处调用了构造方法 public ObjectSubstruct*/
+        System.out.println("小狗的名字是 ：" + name);
+    }
+
+    public static void main(String[] args) {
+        //下面的语句将创建一个Puppy对象
+        ObjectSubstruct myObjectSubstruct = new ObjectSubstruct("June");
+    }
 }
