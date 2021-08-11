9f3a777451e88e0609ae6c5bb155f901e3c85bf5
JTL <1126088799@qq.com>
2021-08-11 23:16:10 +0800

    修改数组

--- java-exercise/src/main/java/com/ljt/base/PrintArray.java
+++ java-exercise/src/main/java/com/ljt/base/PrintArray.java
@@ -1,10 +1,11 @@
 package com.ljt.base;
 
 public class PrintArray {
-
+    public static void main(String[] args) {
+        printArray(new int[]{0,2,6,7});
+    }
     public static void printArray(int[] array){
         for (int i = 0; i < array.length; i ++) {
             System.out.println(array[i]+ "");}
     }
 }
-
