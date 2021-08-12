5464a2efa8a9a6f8f7cb063098161c558bfe6d54
JTL <1126088799@qq.com>
2021-08-13 03:47:25 +0800

    自编程序学习

--- java-exercise/src/main/java/com/ljt/base/BigOrSmall.java
+++ java-exercise/src/main/java/com/ljt/base/BigOrSmall.java
@@ -1,2 +1,17 @@
-package com.ljt.base;public class BigOrSmall {
+package com.ljt.base;
+
+public class BigOrSmall {
+    public static void main(String[] args) {
+        max ( 5, 10);
+    }
+    public static void max(int num, int num2) {
+// 一个void方法的调用一定是一个语句。 所以，它被在main方法第三行以语句形式调用。就像任何以分号结束的语句一样。
+        int result;
+        if (num > num2)
+            result = num;
+        else
+            result = num2;
+        System.out.println(result);
+    }
+
 }
