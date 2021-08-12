1117c307c8e55261808789be36244be987380baa
JTL <1126088799@qq.com>
2021-08-12 21:47:42 +0800

    另外的替换方法

--- java-exercise/src/main/java/com/ljt/base/RegexMatches2.java
+++ java-exercise/src/main/java/com/ljt/base/RegexMatches2.java
@@ -4,15 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexMatches2 {
-    private static String REGEX = "dog";
-    private static String INPUT = "The dog says meow. " + "All dogs say meow.";
-    private static String REPLACE = "cat";
+    private static String REGEX = "a*b";
+    private static String INPUT = "aabfooaabfooabfoobkkk";
+    private static String REPLACE = "-";
 
     public static void main(String[] args) {
-       // Pattern p = Pattern.compile(REGEX);
+        // Pattern p = Pattern.compile(REGEX);
         // get a matcher object
         //Matcher m = p.matcher(INPUT);
-        INPUT = Pattern.compile(REGEX).matcher(INPUT).replaceAll(REPLACE);
-        System.out.println(INPUT);
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(INPUT);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+
+            // INPUT = Pattern.compile(REGEX).matcher(INPUT).replaceAll(REPLACE);
+            m.appendReplacement(sb, REPLACE);
+
+        }
+        m.appendTail(sb);
+        System.out.println(sb.toString());
     }
 }
