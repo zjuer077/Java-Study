e8bbb8c0903ec167334569c51a2c7bbdbcfada0c
JTL <1126088799@qq.com>
2021-08-13 20:07:16 +0800

    OutputStream&InputStream

--- java-exercise/src/main/java/com/ljt/base/fileStreamTest.java
+++ java-exercise/src/main/java/com/ljt/base/fileStreamTest.java
@@ -32,23 +32,25 @@ import java.io.*;
 
 public class fileStreamTest {
     public static void main(String[] args) throws IOException {
-        File f = new File("a.txt");
+        File f = new File("C:\\Users\\Ljt\\Desktop\\a.txt");
         FileOutputStream fop = new FileOutputStream(f);
-//  构建FileOutputStream对象，文件不存在会自动新建
+//  构建FileOutputStream对象，创建一个输出流来写文件,文件不存在会自动新建
         OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
 // 构建OutputStreamWriter对象，参数可以指定编码，默认为操作系统默认编码，windows上是gbk
+// 创建OutputStream 对象完成后，就可以使用下面的方法来写入流或者进行其他的流操作。
         writer.append("中文输入");
         //写入缓冲区
         writer.append("\r\n");
         //换行
         writer.append("English");
+        writer.append("\r\n好好科研，李锦涛");
         //刷新缓存冲，写入到文件，如果下面已经没有写入的内容，直接close也会写入
         writer.close();
 //关闭写入流，同时会把缓冲区内容写入文件，所以上面的注释掉
         fop.close();
 //关闭输出流，释放系统资源
         FileInputStream fip = new FileInputStream(f);
-//构建FileInputStream对象，文件不存在会自动新建
+//构建FileInputStream对象，文件不存在会自动新建,创建一个输入流对象来读取文件
         InputStreamReader reader = new InputStreamReader(fip, "UTF-8");
 //构建FileInputStreamReader对象，编码与写入相同
         StringBuffer sb = new StringBuffer();
@@ -58,10 +60,14 @@ public class fileStreamTest {
         }
         System.out.println(sb.toString());
         reader.close();
+        // 关闭读取流
         fip.close();
+        // 关闭输入流，释放系统资源
     }
 }
 
+//InputStream 读取文件 reader
+//OutputStream 写入文件 writer
 
 /*
 import java.io.File;
