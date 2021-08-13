a3a29d1d8d0052e30626b7b10b594a88d4cad10d
JTL <1126088799@qq.com>
2021-08-13 15:12:01 +0800

    读取目录

--- java-exercise/src/main/java/com/ljt/base/fileStreamTest.java
+++ java-exercise/src/main/java/com/ljt/base/fileStreamTest.java
@@ -1,5 +1,6 @@
 package com.ljt.base;
 
+/*
 import java.io.*;
 
 public class fileStreamTest {
@@ -23,4 +24,66 @@ public class fileStreamTest {
         }
     }
 }
+*/
 
+
+/*
+import java.io.*;
+
+public class fileStreamTest {
+    public static void main(String[] args) throws IOException {
+        File f = new File("a.txt");
+        FileOutputStream fop = new FileOutputStream(f);
+//  构建FileOutputStream对象，文件不存在会自动新建
+        OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+// 构建OutputStreamWriter对象，参数可以指定编码，默认为操作系统默认编码，windows上是gbk
+        writer.append("中文输入");
+        //写入缓冲区
+        writer.append("\r\n");
+        //换行
+        writer.append("English");
+        //刷新缓存冲，写入到文件，如果下面已经没有写入的内容，直接close也会写入
+        writer.close();
+//关闭写入流，同时会把缓冲区内容写入文件，所以上面的注释掉
+        fop.close();
+//关闭输出流，释放系统资源
+        FileInputStream fip = new FileInputStream(f);
+//构建FileInputStream对象，文件不存在会自动新建
+        InputStreamReader reader = new InputStreamReader(fip, "UTF-8");
+//构建FileInputStreamReader对象，编码与写入相同
+        StringBuffer sb = new StringBuffer();
+        while(reader.ready()){
+            sb.append((char)reader.read());
+
+        }
+        System.out.println(sb,toString());
+        reader.close();
+        fip.close();
+    }
+}
+*/
+
+
+import java.io.File;
+
+public class fileStreamTest{
+    public static void main(String[] args) {
+    String dirname = "/desktop";
+    File f1 = new File(dirname);
+    if (f1.isDirectory()){
+        System.out.println("目录" + dirname);
+        String s[] = f1.list();
+        for (int i = 0; i < s.length; i++){
+            File f = new File(dirname + "/" + s[i]);
+            if ( f.isDirectory()){
+                System.out.println(s[i] + "是一个目录");
+
+            }else {
+                System.out.println(s[i] + "是一个文件");
+            }
+        }
+    } else {
+        System.out.println(dirname + "不是一个目录");
+    }
+}
+}
